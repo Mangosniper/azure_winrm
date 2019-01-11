9232825322d8f2e6f85ce681f03d444cf8d8eb44
Mangosniper <Niklasklein25@gmail.com>
2019-01-11 10:39:28 +0100

    Added Logging to ConfigureWinRM.ps1

--- 201-vm-winrm-windows/ConfigureWinRM.ps1
+++ 201-vm-winrm-windows/ConfigureWinRM.ps1
@@ -3,7 +3,10 @@
 #                                                                                                                               #
 #  Description : Configures the WinRM on a local machine                                                                        #
 #                                                                                                                               #
-#  Arguments   : HostName, specifies the FQDN of machine or domain                                                           #
+#  Arguments   : HostName, specifies the FQDN of machine or domain                                                              #
+#                                                                                                                               #
+#  Version     : 1.1                                                                                                            #
+#                                                                                                                               #
 #################################################################################################################################
 
 param
@@ -26,7 +29,7 @@ function Delete-WinRMListener
             if($conf.Contains("HTTPS"))
             {
                 Write-Verbose "HTTPS is already configured. Deleting the exisiting configuration."
-    
+                Log-Write -LogPath $FullLogPath -LineValue "HTTPS is already configured. Deleting the exisiting configuration."
                 winrm delete winrm/config/Listener?Address=*+Transport=HTTPS
                 break
             }
@@ -35,6 +38,7 @@ function Delete-WinRMListener
     catch
     {
         Write-Verbose -Verbose "Exception while deleting the listener: " + $_.Exception.Message
+        Log-Write -LogPath $FullLogPath -LineValue "Exception while deleting the listener: " + $_.Exception.Message
     }
 }
 
@@ -55,6 +59,7 @@ function Create-Certificate
     if(-not $thumbprint)
     {
         throw "Failed to create the test certificate."
+        Log-Write -LogPath $FullLogPath -LineValue "Failed to create the test certificate"
     }
 
     return $thumbprint
@@ -67,13 +72,16 @@ function Configure-WinRMHttpsListener
 
     # Delete the WinRM Https listener if it is already configured
     Delete-WinRMListener
+    Log-Write -LogPath $FullLogPath -LineValue "Deleted the WinRM Https listener if it is already configured"
 
     # Create a test certificate
     $cert = (Get-ChildItem cert:\LocalMachine\My | Where-Object { $_.Subject -eq "CN=" + $hostname } | Select-Object -Last 1)
+    Log-Write -LogPath $FullLogPath -LineValue "Created a test certificate"
     $thumbprint = $cert.Thumbprint
     if(-not $thumbprint)
     {
 	    $thumbprint = Create-Certificate -hostname $HostName
+        Log-Write -LogPath $FullLogPath -LineValue "There was no thumbprint, created one"
     }
     elseif (-not $cert.PrivateKey)
     {
@@ -81,11 +89,13 @@ function Configure-WinRMHttpsListener
         # Delete the certificate
         Remove-Item Cert:\LocalMachine\My\$thumbprint -Force
         $thumbprint = Create-Certificate -hostname $HostName
+        Log-Write -LogPath $FullLogPath -LineValue "There was no private key, created one"
     }
 
     $WinrmCreate= "winrm create --% winrm/config/Listener?Address=*+Transport=HTTPS @{Port=`"$port`";Hostname=`"$hostName`";CertificateThumbprint=`"$thumbPrint`"}"
     invoke-expression $WinrmCreate
     winrm set winrm/config/service/auth '@{Basic="true"}'
+    Log-Write -LogPath $FullLogPath -LineValue "Set winrm authentication to basic"
 }
 
 function Add-FirewallException
@@ -94,9 +104,11 @@ function Add-FirewallException
 
     # Delete an exisitng rule
     netsh advfirewall firewall delete rule name="Windows Remote Management (HTTPS-In)" dir=in protocol=TCP localport=$port
+    Log-Write -LogPath $FullLogPath -LineValue "Deleted existing rule if there was any"
 
     # Add a new firewall rule
     netsh advfirewall firewall add rule name="Windows Remote Management (HTTPS-In)" dir=in action=allow protocol=TCP localport=$port
+    Log-Write -LogPath $FullLogPath -LineValue "Added new rule"
 }
 
 
@@ -104,17 +116,32 @@ function Add-FirewallException
 #                                              Configure WinRM                                                                  #
 #################################################################################################################################
 
+# Start Logging
+. "Logging_Functions.ps1"
+$FullLogPath = "C:\Windows\Temp\ConfigureWinRM.log"
+Log-Start -LogPath "C:\Windows\Temp" -LogName "ConfigureWinRM.log" -ScriptVersion "1.1"
+
 $winrmHttpsPort=443
+Log-Write -LogPath $FullLogPath -LineValue "Value of winrmHttpsPort:"
+Log-Write -LogPath $FullLogPath -LineValue $winrmHttpsPort
 
 # The default MaxEnvelopeSizekb on Windows Server is 500 Kb which is very less. It needs to be at 8192 Kb. The small envelop size if not changed
 # results in WS-Management service responding with error that the request size exceeded the configured MaxEnvelopeSize quota.
+Log-Write -LogPath $FullLogPath -LineValue "Start seting winrm/config"
 winrm set winrm/config '@{MaxEnvelopeSizekb = "8192"}'
+Log-Write -LogPath $FullLogPath -LineValue "Setting winrm/config completed"
 
 # Configure https listener
+Log-Write -LogPath $FullLogPath -LineValue "Start configuring winrmhttpslistener"
 Configure-WinRMHttpsListener $HostName $winrmHttpsPort
+Log-Write -LogPath $FullLogPath -LineValue "Completed configuring winrmhttpslistener"
 
 # Add firewall exception
+Log-Write -LogPath $FullLogPath -LineValue "Start adding firewallexception"
 Add-FirewallException -port $winrmHttpsPort
+Log-Write -LogPath $FullLogPath -LineValue "Completed adding firewallexception"
+
+Log-Finish -LogPath $FullLogPath -NoExit $True
 
 #################################################################################################################################
 #################################################################################################################################
